b9e28d5510f5921e902820aa2beb00330b285e3e
0xAct0r <gizatullininsaf25@gmail.com>
2023-05-09 19:16:19 +0300

    del

---
+++ src/parser.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+enum KindToken {
+    Num,
+    Bool,
+    Str,
+    Greater,
+    Equal,
+    Less,
+    Comma,
+    Comment,
+    Mul,
+    Div,
+    Add,
+    Sub,
+    Func,
+    OBracket,
+    CBracket,
+}
